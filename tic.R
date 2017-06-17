ed802c4479fd6b173f9849fe90a1738788f7777a
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-17 22:30:09 +0200

    tic-ize

---
+++ tic.R
@@ -0,0 +1,22 @@
+get_stage("before_install") %>%
+  add_step(step_hello_world())
+
+get_stage("install") %>%
+  add_step(step_run_code(remotes::install_deps()))
+
+get_stage("script") %>%
+  add_step(step_run_code(rcmdcheck::rcmdcheck()))
+
+get_stage("after_success") %>%
+  add_step(step_run_code(covr::codecov()))
+
+if (ci()$is_tag() && Sys.getenv("BUILD_PKGDOWN") != "") {
+  get_stage("before_deploy") %>%
+    add_step(step_install_ssh_keys()) %>%
+    add_step(step_add_to_known_hosts("github.com")) %>%
+    add_step(step_test_ssh())
+
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+}
