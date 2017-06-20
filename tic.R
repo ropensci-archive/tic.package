5c08fbdf7384cbc46909d949a37ee46399bf6833
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-20 10:40:48 +0200

    only if id_rsa is available

--- tic.R
+++ tic.R
@@ -10,8 +10,9 @@ get_stage("script") %>%
 get_stage("after_success") %>%
   add_step(step_run_code(covr::codecov(quiet = FALSE)))
 
-if (TRUE) {
-  # pkgdown documentation can be built optionally. Example criteria:
+if (Sys.getenv("id_rsa") != "") {
+  # pkgdown documentation can be built optionally. Other example criteria:
+  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
   # - `ci()$is_tag()`: Only for tags, not for branches
   # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
   # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
