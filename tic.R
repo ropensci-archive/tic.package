9155c2da7b829f0dc6261ef3a2a8cddd3f54abc6
pat-s <patrick.schratz@gmail.com>
2019-04-10 21:46:55 +0200

    use pkgdown macro

--- tic.R
+++ tic.R
@@ -1,10 +1,3 @@
 do_package_checks()
 
-if (ci_has_env("id_rsa")) {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy())
-}
+do_build_pkgdown()
